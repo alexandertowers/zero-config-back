1b6a8964244e43b63ec1466dac4d143109e1d46d
alexandertowers <alexanderjtowers@gmail.com>
2025-02-25 15:09:32 +1300

    CORS

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:5173",  # adjust as needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # allow front-end domain(s)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 class DeployRequest(BaseModel):
     message: str
+
 
 @app.post("/api/deploy")
 async def deploy(request: DeployRequest):
